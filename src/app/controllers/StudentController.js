63a32d9e6975d3886e03f9d12e94458b3449290c
Iann Isacksson <iisackssonoliveira@gmail.com>
2019-12-03 19:07:02 -0300

    Validacao dos dados de update

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -17,6 +17,23 @@ class StudentController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      oldEmail: Yup.string().email(),
+      email: Yup.string().when('oldEmail', (oldEmail, field) =>
+        oldEmail ? field.required() : field
+      ),
+      confirmEmail: Yup.string().when('email', (email, field) =>
+        email ? field.required().oneOf([Yup.ref('email')]) : field
+      ),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fais' });
+    }
+
     const { email, oldEmail } = req.body;
 
     const student = await Student.findOne({
