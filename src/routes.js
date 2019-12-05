51f6cccd443ddad32dd3bc5a4db6647de5697440
Iann Isacksson <iisackssonoliveira@gmail.com>
2019-12-05 19:12:05 -0300

    CRUD do model plan

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,5 +14,10 @@ routes.use(authMiddleware);
 
 routes.post('/student', StudentController.store);
 routes.put('/update', StudentController.update);
+
+routes.post('/plan', PlanController.store);
+routes.get('/plan', PlanController.index);
+routes.put('/plan/:id', PlanController.update);
+routes.delete('/plan/:id', PlanController.delete);
 
 export default routes;
